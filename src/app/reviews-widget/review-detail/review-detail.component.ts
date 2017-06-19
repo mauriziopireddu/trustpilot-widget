a837f1e8ae449a8ba3178ab32bdba695af521138
mauriziopireddu <pireddu.maurizio@gmail.com>
2017-06-19 09:05:28 +0200

    vertical widget ok

--- src/app/reviews-widget/review-detail/review-detail.component.ts
+++ src/app/reviews-widget/review-detail/review-detail.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Review } from "app/shared/review";
 
 @Component({
-  selector: 'app-review-detail',
+  selector: 'review-detail',
   templateUrl: './review-detail.component.html',
   styleUrls: ['./review-detail.component.sass']
 })
-export class ReviewDetailComponent implements OnInit {
+export class ReviewDetailComponent {
+  @Input() 
+  review: Review;
 
   constructor() { }
 
-  ngOnInit() {
+
+  getStartsImg():string {
+    if(this.review.starRating != 1)
+      return `assets/${this.review.starRating}-stars-260x48.png`;
+    return `assets/${this.review.starRating}-star-260x48.png`;
   }
 
+  getReviewBody():string {
+    if(this.review.reviewBody.length > 140)
+      return this.review.reviewBody.substr(0, 140) + " [...]";
+    return this.review.reviewBody;
+  }
 }
