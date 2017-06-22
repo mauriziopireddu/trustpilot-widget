483f8d7b98f94449d65d37bf77ea1f226fcadceb
mauriziopireddu <pireddu.maurizio@gmail.com>
2017-06-22 22:53:31 +0200

    organized css

--- src/app/reviews-widget/review-detail/review-star-rating/review-star-rating.component.ts
+++ src/app/reviews-widget/review-detail/review-star-rating/review-star-rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, OnInit, ElementRef } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'review-star-rating',
